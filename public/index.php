dc6d616fad535be24c0f7e4cf0c54dbf0f5c8802
Roman Artemyev <artemyev.ram@gmail.com>
2024-07-17 22:14:35 +0300

    Урок 4

--- public/index.php
+++ public/index.php
@@ -11,11 +11,16 @@ define('PATH', 'http://practice');
 
 require CORE . '/funcs.php';
 
-$uri = trim($_SERVER['REQUEST_URI'], '/');
+$uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], '/');
+
+dump($uri);
+dump($_GET);
+dump($_SERVER['QUERY_STRING']);
+//parse_url('http://practice/about?id=2');
 
 if ($uri === '') {
     require_once CONTROLLERS . '/index.php';
-} elseif ($uri === 'about') {
+} elseif ($uri == 'about') {
     require_once CONTROLLERS . '/about.php';
 }else {
     abort();
